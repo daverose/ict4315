6c772836a682114c8545c435829bc0a625054449
davearose <davearose@gmail.com>
2016-04-25 12:15:52 -0600

    Merge branch 'master' of https://github.com/daverose/ict4315
    # Conflicts:
    #	src/ict4315/unit4/Node.java

--- src/ict4315/unit5/SavingsAccountTest.java
+++ src/ict4315/unit5/SavingsAccountTest.java
@@ -1,23 +1,28 @@
 package ict4315.unit5;
 
 
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 
-public class SavingsAccountTest {
-  SavingsAccount myAccount = new SavingsAccount("40001",-2);
 
-  @Test
+public class SavingsAccountTest {
+
+
+
+@Test (expected = IllegalArgumentException.class)
+    public void dontAllowAccountToBeOpenedWithLessThanOneCent(){
+        SavingsAccount myAccount = new SavingsAccount("40001",0); // Created just to test the constructor.
+
+
+    }
+      @Test
   public void balanceNotNegative() {
+          SavingsAccount myAccount = new SavingsAccount("40001",1);
       assertTrue(myAccount.getBalance() > 0);
     }
-    @Test(expected=IllegalArgumentException.class)
-    public void dontAllowAccountToBeOpenedWithLessThanOneCent(){
-        SavingsAccount myAccount = new SavingsAccount("40001",0);
 
-
-    }
 //    @Test
 //    public void interestRateNotNegative () {
 //

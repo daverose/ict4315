e173b1677112a0fd160ec9a764dad0aa8debaa2f
daverose <davearose@gmail.com>
2016-04-28 20:20:03 -0600

    added main

---
+++ src/ict4315/unit6/Main.java
@@ -0,0 +1,12 @@
+package ict4315.unit6;
+
+/**
+ * Created by daverose on 4/28/16.
+ */
+//Write a Main class that creates a collection of Contact instances, gets a ContactWriter instance from the Factory
+// and then writes the Contacts out.
+public class Main {
+        public static void main(String args[]){
+
+        }
+}

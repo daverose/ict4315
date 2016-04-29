4d4297772a646b2d4795be0dd78efc3e0f57e2b1
daverose <davearose@gmail.com>
2016-04-28 20:12:12 -0600

    Just getting started.

---
+++ src/ict4315/unit6/Contact.java
@@ -0,0 +1,13 @@
+package ict4315.unit6;
+
+/**
+ * Created by daverose on 4/28/16.
+ */
+public class Contact {
+    private String firstName;
+    private String lastNmae;
+
+    public void writeContact(ContactWriter writer){
+        //Make it happen!
+    }
+}

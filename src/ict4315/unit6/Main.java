6f29348a6d36e13b45ea3f69012e812c7a6efec7
daverose <davearose@gmail.com>
2016-04-30 16:15:08 -0600

    Fixed main.

--- src/ict4315/unit6/Main.java
+++ src/ict4315/unit6/Main.java
@@ -6,10 +6,10 @@ package ict4315.unit6;
 //Write a Main class that creates a collection of Contact instances, gets a ContactWriter instance from the Factory
 // and then writes the Contacts out.
 public class Main {
-        public static void main(String args[]){
-            Contact mycontact = Contact.newBuilder();
-            mycontact.firstName("Dave");
-            mycontact.lastName("Rose");
+        public static void main(String[] args){
+            Contact.Builder mycontact = Contact.newBuilder();
+            mycontact.setfirstName("Dave");
+            mycontact.setlastName("Rose");
             mycontact.build();
             System.out.println(mycontact);
 

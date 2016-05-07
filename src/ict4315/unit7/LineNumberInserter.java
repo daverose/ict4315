32e9c6f6d64bb959613172fa601c4dcd545e4d50
drose200 <dave_rose@cable.comcast.com>
2016-05-07 14:36:54 -0600

    Added finally and additional error messages

--- src/ict4315/unit7/LineNumberInserter.java
+++ src/ict4315/unit7/LineNumberInserter.java
@@ -7,6 +7,11 @@ import java.io.*;
 public class LineNumberInserter {
     private String fileName;
 
+    public String setFileName(String name) {
+        fileName = name;
+        return fileName;
+    }
+
     public  DataInputStream getDataIn(){
         DataInputStream dataIn = new DataInputStream(getFileinputStream(fileName));
         return dataIn;
@@ -18,30 +23,45 @@ public class LineNumberInserter {
     }
 
     public FileOutputStream getFileOutputStream(String name) {
+        FileOutputStream outputStream = null;
         try {
-            FileOutputStream outputStream = new FileOutputStream(name);
+            outputStream = new FileOutputStream(name);
             return outputStream;
         } catch (FileNotFoundException e) {
             System.out.println("The file $ for writing contents to was not found." + name);
             e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    System.out.println("Error getting fileoutputstream() " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
         }
         return null;
     }
 
     public FileInputStream getFileinputStream(String name) {
+        FileInputStream inputStream = null;
         try {
-            FileInputStream inputStream = new FileInputStream(name);
+            inputStream = new FileInputStream(name);
             return inputStream;
         } catch (FileNotFoundException e) {
             System.out.println("The file $ for reading was not found." + name);
             e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e){
+                    System.out.println("Couldn't close file. " + e.getMessage());
+                    e.printStackTrace();;
+                }
+            }
+
         }
         return null;
     }
-    public String setFileName(String name) {
-        fileName = name;
-        return fileName;
-    }
-
-
 }

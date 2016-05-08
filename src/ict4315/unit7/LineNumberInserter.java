82b61b62fbf1ed2eb40ffafbd5259ab26303e952
drose200 <dave_rose@cable.comcast.com>
2016-05-08 17:53:57 -0600

    backing up work

--- src/ict4315/unit7/LineNumberInserter.java
+++ src/ict4315/unit7/LineNumberInserter.java
@@ -3,14 +3,36 @@ package ict4315.unit7;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class LineNumberInserter {
-    public void insertLineNumbers(DataInputStream dataIn, DataOutputStream dataOut){
+    public void insertLineNumbers() {
+        Lines lines = new Lines();
+        LineNumberInserter insterter = new LineNumberInserter();
+        lines.setLines();
+        String fileIn = lines.fivelines;
+        String writeTo = lines.sevenLines;
+        DataStream stream = new DataStream();
+        DataInputStream dataIn = stream.getDataIn(fileIn);
+        DataOutputStream dataOut = stream.getDataOut(writeTo);
+        try {
+            dataOut.flush();
+        } catch (IOException e) {
+            System.out.println("No flush() !");
+        }
+        try {
+            StringBuffer in = new StringBuffer();
+            String tmp;
+            try {
+                while ((dataIn.readLine()) != null)
+                    in.append(tmp);
+                System.out.println(tmp);
+            } catch (IOException ioe) {
+                System.out.println(ioe);
+            } catch (NullPointerException n) {
+                System.out.print("Null pointer");
 
-        System.out.println(dataIn);
-        System.out.println(dataOut);
-
-
-
+            }
+        }
     }
 }
--- src/ict4315/unit7/Main.java
+++ src/ict4315/unit7/Main.java
@@ -7,15 +7,17 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Lines lines = new Lines();
-        LineNumberInserter insterter = new LineNumberInserter();
-        lines.setLines();
-        String fileIn = lines.fivelines;
-        String writeTo = lines.sevenLines;
-        DataStream stream = new DataStream();
-        DataInputStream dataIn = stream.getDataIn(fileIn);
-        DataOutputStream dataOut = stream.getDataOut(writeTo);
-        insterter.insertLineNumbers(dataIn, dataOut);
+
 
     }
 }
+//
+//    Lines lines = new Lines();
+//    LineNumberInserter insterter = new LineNumberInserter();
+//lines.setLines();
+//        String fileIn = lines.fivelines;
+//        String writeTo = lines.sevenLines;
+//        DataStream stream = new DataStream();
+//        DataInputStream dataIn = stream.getDataIn(fileIn);
+//        DataOutputStream dataOut = stream.getDataOut(writeTo);
+//        insterter.insertLineNumbers(dataIn, dataOut);

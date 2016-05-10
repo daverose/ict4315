e7f4bc84f389bd1f0f6cbfa2cdfd3468479fda5b
drose200 <dave_rose@cable.comcast.com>
2016-05-10 17:32:38 -0600

    Implimenting insertlines with Scanner

--- src/ict4315/unit7/LineNumbers.java
+++ src/ict4315/unit7/LineNumbers.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
+
 
 public class LineNumbers {
     FileReader fileReader;
@@ -15,8 +17,8 @@ public class LineNumbers {
             FileReader fileReader = new FileReader(argsZero);
             return fileReader;
         } catch (FileNotFoundException fnf) {
-            System.out.println("The file you name you typed as the first argument does not exist as you " +
-                    "typed it in. If the file has an extension, that extension must also be included such as .txt");
+            System.out.println("The file you name you typed as the first argument does not exist as you" +
+                    " typed it in. If the file has an extension, that extension must also be included such as .txt");
         }
         return fileReader;
     }
@@ -27,8 +29,8 @@ public class LineNumbers {
         if (f.exists() && !f.isDirectory()) {
             f.delete();
         } else if (f.isDirectory()) {
-            System.out.println("The second argument to the command you entered is a directory name and " +
-                    "must be changed to a name that is not a directory");
+            System.out.println("The second argument to the command you entered is a directory name and" +
+                    " must be changed to a name that is not a directory");
         }
         try {
             FileWriter fileWriter = new FileWriter(f);
@@ -38,12 +40,15 @@ public class LineNumbers {
         } catch (IOException ioe) {
             System.out.println("The file could not be created. A check was already made to see if the file name" +
                     "you entered was a directory and if the file already existed. Perhaps you don't have permissions to write" +
-                    "in the location you've chosen?");
+                    " in the location you've chosen?");
             ioe.printStackTrace();
         }
         return fileWriter;
     }
     public void insertLineNumbers(FileReader inputFile){
+        int lineNumber = 0;
+        Scanner sc = new Scanner(inputFile);
+
 
     }
 }

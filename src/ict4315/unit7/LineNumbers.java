876cd2f9da6f9124cd3931d9d42ce2b2722376c7
drose200 <dave_rose@cable.comcast.com>
2016-05-10 18:48:16 -0600

    Program working from CLI. Strange behavor when running from IDE.

--- src/ict4315/unit7/LineNumbers.java
+++ src/ict4315/unit7/LineNumbers.java
@@ -6,16 +6,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.regex.MatchResult;
 
 
 public class LineNumbers {
     FileReader fileReader;
-    FileWriter fileWriter;
 
     public FileReader getInputFile(String argsZero) {
+        String filePath = new File("").getAbsolutePath();
+        String pathSeparator = System.getProperty("file.separator");
+        System.out.println("The following is your path." + pathSeparator.toString());
+        System.out.println(filePath.concat(pathSeparator + argsZero));
         try {
-            FileReader fileReader = new FileReader(argsZero);
+            FileReader fileReader = new FileReader(filePath.concat(pathSeparator + argsZero));
             return fileReader;
         } catch (FileNotFoundException fnf) {
             System.out.println("The file you name you typed as the first argument does not exist as you" +
@@ -24,29 +26,7 @@ public class LineNumbers {
         return fileReader;
     }
 
-    public FileWriter getOutputFile(String argsOne) {
-        Lines lines = new Lines();
-        File f = new File(argsOne);
-        if (f.exists() && !f.isDirectory()) {
-            f.delete();
-        } else if (f.isDirectory()) {
-            System.out.println("The second argument to the command you entered is a directory name and" +
-                    " must be changed to a name that is not a directory");
-        }
-        try {
-            FileWriter fileWriter = new FileWriter(f);
-            fileWriter.write(lines.fivelines());
-            fileWriter.close();
-            return fileWriter;
-        } catch (IOException ioe) {
-            System.out.println("The file could not be created. A check was already made to see if the file name" +
-                    "you entered was a directory and if the file already existed. Perhaps you don't have permissions to write" +
-                    " in the location you've chosen?");
-            ioe.printStackTrace();
-        }
-        return fileWriter;
-    }
-    public void insertLineNumbers(FileReader inputFile, String argsOne){
+     public void insertLineNumbers(FileReader inputFile, String argsOne){
         int i =0;
         StringBuilder newString = new StringBuilder();
         Scanner sc = new Scanner(inputFile).useDelimiter("\r\n");
--- src/ict4315/unit7/Main.java
+++ src/ict4315/unit7/Main.java
@@ -1,26 +1,13 @@
 package ict4315.unit7;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.FileReader;
 
 public class Main {
-
-
     public static void main(String[] args) {
-    Lines lines = new Lines();
-    LineNumbers lineNumbers = new LineNumbers();
-        lines.setLines();
-        String fileIn = lines.fivelines;
-        String writeTo = lines.sevenLines;
-//        DataStream stream = new DataStream();
-//        DataInputStream dataIn = stream.getDataIn(fileIn);
-//        DataOutputStream dataOut = stream.getDataOut(writeTo);
-        lineNumbers.getInputFile(args[0]);
-       // lineNumbers.getOutputFile(args[1]);
-        lineNumbers.insertLineNumbers(lineNumbers.getInputFile(args[0]), args[1]);
-
-
-
+        FileReader inputFile;
+        LineNumbers lineNumbers = new LineNumbers();
+        inputFile = lineNumbers.getInputFile(args[0]);
+        lineNumbers.insertLineNumbers(inputFile, args[1]);
     }
 }
 

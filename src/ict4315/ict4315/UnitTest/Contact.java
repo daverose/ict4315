eab401bf82f6f0c7ef2815497a336a635724867c
drose200 <dave_rose@cable.comcast.com>
2016-05-17 10:52:56 -0600

    Adding assignment 8

---
+++ src/ict4315/ict4315/UnitTest/Contact.java
@@ -0,0 +1,7 @@
+package ict4315.ict4315.UnitTest;
+
+/**
+ * Created by drose200 on 5/17/2016.
+ */
+public class Contact {
+}
---
+++ src/ict4315/ict4315/UnitTest/ContactTest.java
@@ -0,0 +1,25 @@
+package ict4315.ict4315.UnitTest;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+public class ContactTest {
+
+    public void createAddressBook() {
+
+    }
+    public void addContact() {
+
+    }
+
+    public void deleteContact() {
+
+    }
+    public void search() {
+
+    }
+    public void edit() {
+
+    }
+}

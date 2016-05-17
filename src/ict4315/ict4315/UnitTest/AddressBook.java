6c31744f3b360b9fe0a3e03fab8123645d6b5a83
drose200 <dave_rose@cable.comcast.com>
2016-05-17 11:46:40 -0600

    Adding Unit Tests

--- src/ict4315/ict4315/UnitTest/AddressBook.java
+++ src/ict4315/ict4315/UnitTest/AddressBook.java
@@ -3,10 +3,10 @@ package ict4315.ict4315.UnitTest;
 import java.util.ArrayList;
 public class AddressBook {
     ArrayList addressBook = new ArrayList<Contact>();
-    Contact contact = new Contact();
-
-contact.createContact()
-
-
+    Contact contact = new Contact.ContactBuilder()
+            .firstName("Dave")
+            .lastName("Rose")
+            .email("davearose@gmail.com")
+            .build();
 
 }

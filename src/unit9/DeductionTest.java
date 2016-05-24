5d705dd558864287c44c83885db5a917fb69bd1d
daverose <davearose@gmail.com>
2016-05-24 16:22:46 -0600

    Added deduction class.

---
+++ src/unit9/DeductionTest.java
@@ -0,0 +1,37 @@
+package unit9;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by daverose on 5/24/16.
+ */
+public class DeductionTest {
+    @Test
+    public void deductCharityContributions() {
+
+
+    }
+
+    @Test
+    public void deductHomeInterest() {
+
+    }
+
+    @Test
+    public void deductStockWashSale() {
+
+    }
+
+    @Test
+    public void deductStandardSingle() {
+
+    }
+
+    @Test
+    public void deductStandardMarried() {
+
+    }
+
+}
---
+++ src/unit9/PersonalDeduction.java
@@ -0,0 +1,20 @@
+package unit9;
+
+public class PersonalDeduction implements Deduction{
+    private int deduction;
+    public void deductChartityContribution() {
+
+    }
+    private void deductHomeInsterest(){
+
+    }
+    private void deductStockWashSale(){
+
+    }
+    private void deductStandardSingle() {
+
+    }
+    private void deductStandardMarried() {}
+
+
+}

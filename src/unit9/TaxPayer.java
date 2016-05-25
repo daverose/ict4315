4888df9b96e41704a825e7056ee92d73bcd76e47
daverose <davearose@gmail.com>
2016-05-24 18:35:58 -0600

    Added taxpayer class.

---
+++ src/unit9/TaxPayer.java
@@ -0,0 +1,55 @@
+package unit9;
+
+public class TaxPayer {
+    private int totalIncome;
+    private int totalDeductions;
+    private int adjustedIncome;
+    private PersonalDeduction deduction;
+    private PersonalIncome income;
+    private boolean married = false;
+    private int taxOwed;
+    private boolean taxOwedSet = false;
+    private boolean setAdjIncome = false;
+
+    public TaxPayer(){
+        deduction = new PersonalDeduction();
+        income = new PersonalIncome();
+    }
+    public TaxPayer(boolean married){
+        this.married = married;
+        deduction = new PersonalDeduction();
+        income = new PersonalIncome();
+    }
+
+    public void setMaritalStatus(boolean married) {
+        this.married = married;
+    }
+    public int getTotalIncome() {
+        return income.getPersonalIncome();
+    }
+    public int getTotalDeductions() {
+        return deduction.getDeductions();
+    }
+    public int getAdjustedIncome() throws Exception {
+        if (setAdjIncome) {
+            return adjustedIncome;
+        } else {
+            throw new Exception("Adjusted Income must be set before it can be called.");
+        }
+    }
+    public void setAdjustedIncome() {
+        setAdjIncome = true;
+        adjustedIncome = totalIncome - totalDeductions;
+    }
+    public void setTaxOwed(int tax) {
+        taxOwedSet = true;
+        taxOwed = tax;
+    }
+    public int getTaxOwed() throws Exception {
+        if ( taxOwedSet) {
+            return taxOwed;
+        } else {
+            throw new Exception("The taxOwed must be set before it can be returned");
+        }
+    }
+}
